4bb6f9f273ec23e32cc40cf461d4417e0a2ec85b
icarolinear <icaro.adao@linearsistemas.com.br>
2019-11-05 14:31:18 -0200

    teste monorepo com lerna

---
+++ packages/grocery/index.js
@@ -0,0 +1,6 @@
+const sillyname = require('sillyname');
+const apple = require('apple');
+const orange = require('orange');
+console.log(`grocery and ${sillyname()}`);
+console.log(apple);
+console.log(orange);
